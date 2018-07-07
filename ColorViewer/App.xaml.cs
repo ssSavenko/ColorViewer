d3b0170f36557d33d0b8f0900f85a6a087fcd134
ss.savenko <ss.savenko@gmail.com>
2018-07-07 16:37:03 +0100

    Add project files.

---
+++ ColorViewer/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ColorViewer.ModelViewers;
+using ColorViewer.Views;
+
+namespace ColorViewer
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+
+    internal partial class App : Application
+    {
+        public App()
+        {
+            Startup += AppStarup;
+        }
+        
+        public void AppStarup(object sender, StartupEventArgs e)
+        {
+            var viewModel = new MainViewModel();
+            var view = new MainWindow(viewModel);
+
+            view.Show();
+        }
+    }
+}
---
+++ ColorViewer/Models/MyColor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorViewer.ModelViewers
+{
+    internal class MyColor
+    {
+        private int alpha;
+        private int red;
+        private int green;
+        private int blue;
+
+        public int Alpha
+        {
+            get { return alpha; }
+            set
+            {
+                if (0 < value && value <= 255)
+                {
+                    alpha = value;
+                }
+            }
+        }
+    }
+}
