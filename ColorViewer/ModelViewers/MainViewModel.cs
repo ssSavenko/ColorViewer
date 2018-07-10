c90e0e497114b08479aa1aad89a2b9a4fff08ea8
ss.savenko <ss.savenko@gmail.com>
2018-07-10 16:52:38 +0300

    Save

--- ColorViewer/ModelViewers/MainViewModel.cs
+++ ColorViewer/ModelViewers/MainViewModel.cs
@@ -3,14 +3,126 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace ColorViewer.ModelViewers
 {
-    internal class MainViewModel
+    internal class MainViewModel : INotifyPropertyChanged
     {
+        private MyColor currentColor;
+        private bool isAlphaSliderEnable;
+        private bool isBlueSliderEnable;
+        private bool isGreenSliderEnable;
+        private bool isRedSliderEnable;
+        private ObservableCollection<MyColor> SavedColors;
+
         public MainViewModel()
         {
+            currentColor = new MyColor();
+            currentColor.Alpha = 255;
+            SavedColors = new ObservableCollection<MyColor>();
+            SavedColors.Add(currentColor);
+        }
 
+        public bool IsAlphaSliderEnable
+        {
+            get { return !isAlphaSliderEnable; }
+            set { isAlphaSliderEnable = value;
+                OnPropertyChanged(new PropertyChangedEventArgs (nameof(IsAlphaSliderEnable)));
+            }
+        }
+
+        public bool IsBlueSliderEnable
+        {
+            get { return !isBlueSliderEnable; }
+            set
+            {
+                isBlueSliderEnable = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(IsBlueSliderEnable)));
+            }
+        }
+
+        public bool IsGreenSliderEnable
+        {
+            get { return !isGreenSliderEnable; }
+            set
+            {
+                isGreenSliderEnable = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(IsGreenSliderEnable)));
+            }
+        }
+
+        public bool IsRedSliderEnable
+        {
+            get { return !isRedSliderEnable; }
+            set
+            {
+                isRedSliderEnable = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(IsRedSliderEnable)));
+            }
+        }
+
+        public int CurrentAlpha
+        {
+            get { return currentColor.Alpha; }
+            set
+            {
+                currentColor.Alpha = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(CurrentAlpha)));
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(currentColor)));
+                string mu = currentColor.ColorInHex;
+            }
+        }
+
+        public int CurrentBlueColor
+        {
+            get { return currentColor.Blue; }
+            set
+            {
+                currentColor.Blue = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(CurrentBlueColor)));
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(currentColor)));
+            }
+        }
+
+        public string CurrentColor
+        {
+            get { return currentColor.ColorInHex; }
+        }
+
+        public int CurrentGreenColor
+        {
+            get { return currentColor.Green; }
+            set
+            {
+                currentColor.Green = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(CurrentGreenColor)));
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(currentColor)));
+            }
+        }
+
+        public int CurrentRedColor
+        {
+            get { return currentColor.Red; }
+            set
+            {
+                currentColor.Red = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(CurrentRedColor)));
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(currentColor)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
         }
     }
 }
--- ColorViewer/Models/MyColor.cs
+++ ColorViewer/Models/MyColor.cs
@@ -9,19 +9,100 @@ namespace ColorViewer.ModelViewers
     internal class MyColor
     {
         private int alpha;
-        private int red;
-        private int green;
         private int blue;
+        private int green;
+        private int red;
+
+        public MyColor()
+        {
+            alpha = 0;
+            blue = 0;
+            green = 0;
+            red = 0;
+        }
 
         public int Alpha
         {
             get { return alpha; }
             set
             {
-                if (0 < value && value <= 255)
+                if (0 <= value && value <= 255)
                 {
                     alpha = value;
                 }
+            }
+        }
+
+        public int Blue
+        {
+            get { return blue; }
+            set
+            {
+                if (0 <= value && value <= 255)
+                {
+                    blue = value;
+                }
+            }
+        }
+
+        public int Green
+        {
+            get { return green; }
+            set
+            {
+                if (0 <= value && value <= 255)
+                {
+                    green = value;
+                }
+            }
+        }
+
+        public int Red
+        {
+            get { return red; }
+            set
+            {
+                if (0 <= value && value <= 255)
+                {
+                    red = value;
+                }
+            }
+        }
+
+        public string ColorInHex
+        {
+            get
+            {
+                string hexValue = "#";
+                string alphaValue = alpha.ToString("X");
+
+                if(alphaValue.Length < 2)
+                {
+                    hexValue += "0";
+                }
+                hexValue += alphaValue;
+
+                string redValue = red.ToString("X");
+                if (redValue.Length < 2)
+                {
+                    hexValue += "0";
+                }
+                hexValue += redValue;
+
+                string greenValue = green.ToString("X");
+                if (greenValue.Length < 2)
+                {
+                    hexValue += "0";
+                }
+                hexValue += greenValue;
+
+                string blueValue = blue.ToString("X");
+                if (blueValue.Length < 2)
+                {
+                    hexValue += "0";
+                }
+                hexValue += blueValue;
+                return hexValue;
             }
         }
     }
